975e3d1f57764b1c429ca38cc29dc8cd9d0ff077
Mariem <mariem.ehab@ieee.org>
2023-11-28 08:18:17 -0800

    8. Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(\+\d{10})|(-?[0-1]:-?[0-1]:-?[0-1]:-?[0-1]:-?[0-1])/).flatten.compact.join(',')
+puts ARGV[0].scan(/(\[from:([^\]]+)\])|(\[to:([^\]]+)\])|(-?[0-1]:-?[0-1]:-?[0-1]:-?[0-1]:-?[0-1])/).flatten.compact.values_at(1,3,4).join(',')
