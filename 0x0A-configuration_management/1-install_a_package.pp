805137a564c614056afba9f600e781858981c299
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-19 08:55:48 -0800

    Task 1 also adding insall of pip and wekzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,20 @@
 # 1. Install a package
 
+exec { 'install_python3_pip':
+  command => '/usr/bin/apt-get install -y python3-pip',
+  path    => '/usr/bin',
+}
+
 exec { 'install_flask':
-  command => '/bin/pip3 install Flask==2.1.0',
-  path    => '/bin',
-  unless  => '/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Exec['install_python3_pip'],
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+  require => Exec['install_python3_pip'],
 }
