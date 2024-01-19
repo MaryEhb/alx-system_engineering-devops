8ab6144354c4813e8b0476e990ba70e0fa52a063
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-19 06:14:44 -0800

    Task 1 go back to using package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # 1. Install a package
 
-exec { 'flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    creates => '/usr/local/bin/flask',
-    require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
