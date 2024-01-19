0b78280407f600df3a7804d0491301de8719520f
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-19 05:58:43 -0800

    Task 1 by exec

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # 1. Install a package
 
 exec { 'flask':
-    command => 'pip3 install flask==2.1.0',
+    command => '/usr/bin/pip3 install flask==2.1.0',
     creates => '/usr/local/bin/flask'
     require => Package['python3-pip'],
 }
