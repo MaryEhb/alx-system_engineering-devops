31ceba20446a588d52f44d05cfb94312724b9494
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-19 06:21:59 -0800

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,5 +4,4 @@ exec { 'install_flask':
   command => '/bin/pip3 install Flask==2.1.0',
   path    => '/bin',
   unless  => '/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
 }
