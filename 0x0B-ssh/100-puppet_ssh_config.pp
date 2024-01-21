bb7522a88eed4f2962df595117b637a3590143f7
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-21 03:50:54 -0800

    Task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+# 4. Client configuration file (w/ Puppet)
+
+
+# Disable PasswordAuthentication in sshd_config
+file_line{ 'disable_password_auth':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
+}
+
+# SSH client configuration must be configured to use the
+# private key ~/.ssh/school
+file_line{ 'use_school_private_key':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
