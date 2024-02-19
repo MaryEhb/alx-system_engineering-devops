8ee7795ce95460bae78b83d616f39dd6d5866dc9
Your Name <you@example.com>
2024-02-19 06:46:23 -0800

    Task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""1. Export to CSV"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    import csv
+
+    if (sys.argv[1]):
+        urlTODO = 'https://jsonplaceholder.typicode.com/todos?userId='
+        urlUSER = 'https://jsonplaceholder.typicode.com/users?id='
+        res = requests.get(urlTODO + sys.argv[1]).json()
+        user = requests.get(urlUSER + sys.argv[1]).json()[0]
+        completed = [obj for obj in res if obj.get('completed')]
+
+        with open("{}.csv".format(user.get('id')), 'w', newline='') as file:
+            writer = csv.writer(file)
+            for todo in res:
+                writer.writerow([(todo.get('userId')),
+                                 user.get('name'),
+                                 todo.get('completed'),
+                                 todo.get('title')])
