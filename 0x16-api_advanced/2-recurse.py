bec8f27e01e82d6a63ec0767e291541388a21ed6
Mariem Ehab <mariem.ehab234@gmail.com>
2024-04-09 08:11:17 -0700

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
 """2. Recurse it!"""
-import requests
 
 
 def recurse(subreddit, hot_list=[]):
     """queries the Reddit API and prints the titles of
     the first 10 hot posts listed for a given subreddit."""
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-    params = {"count": 0, "after": None}
-    result = requests.get(url, headers=headers,
-                          params=params, allow_redirects=False)
-    if result.status_code >= 400:
-        return None
-
-    info = result.json()
-    result = hot_list + [child.get("data").get("title")
-                         for child in result.json()
-                         .get("data")
-                         .get("children")]
-    if not info.get("data").get("after"):
-        return result
-    return recurse(subreddit, result, info.get("data").get("count"),
-                   info.get("data").get("after"))
+    pass
