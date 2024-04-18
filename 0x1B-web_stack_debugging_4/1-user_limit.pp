29f0d29e457cecb051632562d61c717ecbc59ff0
Mariem Ehab <mariem.ehab234@gmail.com>
2024-04-18 08:46:02 +0000

    1. User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# 1. User limit
+
+
+exec { 'command1':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'command2':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
